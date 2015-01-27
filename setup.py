b14071e6401509ba755c4049a00984191001de5b
Eko Prasetya <eko.prasetya@cyaninc.com>
2015-01-27 11:50:52 -0800

    Fixed setup.py to allow pip install
    Signed-off-by: Eko Prasetya <eko.prasetya@cyaninc.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 long_desc=open('README.rst').read()
 
-setup(name="datomic",
+setup(name="pydatomic",
       version="0.1.0",
       author="Graham Stratton",
       author_email="gns24@beasts.org",
@@ -14,7 +14,6 @@ setup(name="datomic",
       url="https://github.com/gns24/pydatomic",
       install_requires=['requests'],
       license='mit',
-      py_modules=['datomic', 'edn'],
       classifiers=['Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
